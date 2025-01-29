49650de6ecec654e54826a0a8d4b022fcd6ca30e
Pratik Thorat <thorat.p@northeastern.edu>
2025-01-29 11:15:44 -0500

    Folder Restructuring

--- Basics/basic_math/basicMathThoery.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-// digit extraction is done by number % 10. The remainder is always the last digit.
-// so basically, use n = n/10 in loop and that will give you the count of the digits in a number. to keep extracting digits of the number use the remainder of the number. so it will look like this:
-// while (number > 0) {
-//   last digit = n % 10;
-// }
