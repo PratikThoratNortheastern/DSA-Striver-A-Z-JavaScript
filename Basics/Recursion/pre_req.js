4a9929e98565b7db9b51a531a69d520afbd5f7ca
Pratik Thorat <thorat.p@northeastern.edu>
2025-02-07 16:26:26 -0500

    ADDS RECURSION

---
+++ Basics/Recursion/pre_req.js
@@ -0,0 +1,14 @@
+"use strict";
+/*
+The approach:
+Problem statement will be given.
+Pause the video and try to solve the problem
+Implement the solution together
+Determine the Big O of the solution
+*/
+
+// Fibonnaci Sequence
+// Given a number 'n' find the first 'n' elements of the Fibonacci sequence
+// A Fibonacci sequence is a sequence in which each number is the sum of the two preceding numbers.
+// first two numbers are 0 and 1
+// Fibonacci(2):
