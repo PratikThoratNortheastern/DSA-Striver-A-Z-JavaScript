3fc62f1158e8e9a12fc34a222cf71c7c6f35fb19
Pratik Thorat <thorat.p@northeastern.edu>
2025-02-11 17:29:15 -0500

    Basic rules and syntax

--- Basics/Recursion/recursion.js
+++ Basics/Recursion/recursion.js
@@ -1,1 +1,16 @@
 "use strict";
+// Few points
+// Every recursive solution needs to have a base case to break out of the recursion.
+// Recursion may simplufy solving a problem but it is not always the fastest solution.
+// Sometimes it is slower compared to the iterative  solution.
+
+// Syntax:
+
+function recursiveFunc(n) {
+  if (n === 0) return;
+
+  console.log(n); // Print before recursion
+  recursiveFunc(n - 1);
+}
+
+recursiveFunc(4);
